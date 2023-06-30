ed433da8c942b36fbb9c30e0c7cb4e85979c90b8
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-06-30 10:04:11 -0400

    move classes

---
+++ src/main/java/mc/minigame/listener/Shoot.java
@@ -0,0 +1,54 @@
+package mc.minigame.listener;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.util.Vector;
+
+public class Shoot implements Listener{
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+        // Check if the player right-clicked with a diamond hoe
+        if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) &&
+                event.hasItem() && event.getItem().getType() == Material.DIAMOND_HOE) {
+
+            player.playSound(player.getLocation(), "block.mud_bricks.break", 1.0f, 1.0f);
+
+            // Calculate the crosshair target vector based on the player's location and direction
+            Vector direction = player.getEyeLocation().getDirection();
+            double range = 100;
+            Location targetLocation = player.getEyeLocation().clone();
+
+            for (int i = 0; i < range; i++) {
+                targetLocation.add(direction);
+
+                for (Entity target : player.getWorld().getEntities()) {
+                    if ((target.getLocation().getBlock().getX() == targetLocation.getBlock().getX()) && (target.getLocation().getBlock().getZ() == targetLocation.getBlock().getZ())
+                            && (target.getLocation().getBlock().getY() >= targetLocation.getBlock().getY() - (target.getHeight()) &&
+                            target.getLocation().getBlock().getY() <= targetLocation.getBlock().getY() + (target.getHeight()))) {
+                        if (target instanceof LivingEntity && !(target instanceof Player)) {
+                            LivingEntity livingEntity = (LivingEntity) target;
+                            double damageAmount = 5.0;
+                            livingEntity.damage(damageAmount, player);
+                        }
+                        break;
+                    }
+                }
+
+                // Check if the target location is obstructed by a block
+                if (targetLocation.getBlock().getType().isSolid()) {
+                    break;
+                }
+            }
+        }
+    }
+}
