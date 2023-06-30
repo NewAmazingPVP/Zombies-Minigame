3c89bcc9a07469c459d729805af9b63d23dd06aa
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-06-30 09:51:51 -0400

    new packages

--- src/main/java/mc/minigame/Attack.java
+++
@@ -1,9 +0,0 @@
-package mc.minigame;
-
-import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffectType;
-
-public class Attack {
-    public static void sendZombies(Player player) {
-    }
-}
