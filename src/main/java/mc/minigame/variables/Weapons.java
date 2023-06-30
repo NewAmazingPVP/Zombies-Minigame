ed7134d5c908d42ead3ca19cc09a2379d1d424e8
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-06-30 15:06:26 -0400

    class changes for standardization
    class changes for standardization

--- src/main/java/mc/minigame/variables/Weapons.java
+++ src/main/java/mc/minigame/variables/Weapons.java
@@ -1,11 +1,19 @@
 package mc.minigame.variables;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Weapons {
+
+    private double defaultDamageAmount = 5.0;
+    private double defaultCooldown = 1.0;
+    public Map<Player, Long> cooldowns = new HashMap<>();
+
     public List<Material> weaponList = Arrays.asList(
             Material.WOODEN_HOE,
             Material.STONE_HOE,
@@ -14,6 +22,47 @@ public class Weapons {
             Material.DIAMOND_HOE,
             Material.NETHERITE_HOE
     );
+
+
+    public double getCooldownDuration(Material weaponType) {
+        // Add custom cooldown duration based on the weapon type
+        switch (weaponType) {
+            case WOODEN_HOE:
+                return 0.5; // 0.5 seconds
+            case STONE_HOE:
+                return 0.6; // 0.6 seconds
+            case IRON_HOE:
+                return 0.7; // 0.7 seconds
+            case GOLDEN_HOE:
+                return 0.8; // 0.8 seconds
+            case DIAMOND_HOE:
+                return 0.9; // 0.9 seconds
+            case NETHERITE_HOE:
+                return 1.0; // 1.0 second
+            default:
+                return defaultCooldown;
+        }
+    }
+
+    public double calculateDamageAmount(Material weaponType) {
+        // Add custom damage calculation based on the weapon type
+        switch (weaponType) {
+            case WOODEN_HOE:
+                return 4.0;
+            case STONE_HOE:
+                return 5.0;
+            case IRON_HOE:
+                return 6.0;
+            case GOLDEN_HOE:
+                return 7.0;
+            case DIAMOND_HOE:
+                return 8.0;
+            case NETHERITE_HOE:
+                return 9.0;
+            default:
+                return defaultDamageAmount;
+        }
+    }
 
     public Weapons(){
 
