d604492bdca7ced3133da13e6553bcb986dbeaca
NewAmazingPVP <gamingofpat@gmail.com>
2023-07-10 22:51:37 -0400

    no flickering scoreboard

--- src/main/java/mc/minigame/Zombies.java
+++ src/main/java/mc/minigame/Zombies.java
@@ -1,24 +1,32 @@
 package mc.minigame;
 
 import mc.minigame.command.*;
-import mc.minigame.game.DisplayBoard;
+import mc.minigame.game.Rounds;
+import mc.minigame.game.ZombieCount;
 import mc.minigame.game.PlayerMoney;
 import mc.minigame.listener.*;
+import mc.minigame.scoreboard.FastBoard;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 
 public class Zombies extends JavaPlugin implements Listener {
     public static Economy economy;
     public static Zombies zombies;
+    private final Map<UUID, FastBoard> boards = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -37,6 +45,11 @@ public class Zombies extends JavaPlugin implements Listener {
         getCommand("startgame").setExecutor(new Start());
         getCommand("stopgame").setExecutor(new Stop());
         setupEconomy();
+        getServer().getScheduler().runTaskTimer(this, () -> {
+            for (FastBoard board : this.boards.values()) {
+                updateBoard(board);
+            }
+        }, 0, 20);
     }
 
     @EventHandler
@@ -47,7 +60,18 @@ public class Zombies extends JavaPlugin implements Listener {
         PotionEffect newEffect = new PotionEffect(PotionEffectType.BLINDNESS, 5*20, 1, false, false);
         player.addPotionEffect(newEffect);
         PlayerMoney.setCoins(player, 0);
-        DisplayBoard.board();
+        FastBoard board = new FastBoard(player);
+        board.updateTitle(ChatColor.DARK_PURPLE + "Zombies Minigame");
+        this.boards.put(player.getUniqueId(), board);
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+        FastBoard board = this.boards.remove(player.getUniqueId());
+        if (board != null) {
+            board.delete();
+        }
     }
 
     public static void setupEconomy() {
@@ -57,4 +81,20 @@ public class Zombies extends JavaPlugin implements Listener {
         }
     }
 
+    private void updateBoard(FastBoard board) {
+        int coins = PlayerMoney.getCoins(board.getPlayer());
+        int level = Rounds.getRounds();
+        int ammo = Shoot.countArrowsInInventory(board.getPlayer());
+        int seconds = Rounds.getTimeLeft();
+        int zombieCount = ZombieCount.getTotalZombieCount();
+
+        board.updateLines(
+                "",
+                ChatColor.YELLOW + "Coins: " + coins,
+                ChatColor.AQUA + "Level: " + level,
+                ChatColor.BLUE + "Ammo: " + ammo,
+                ChatColor.GREEN + "Time Left: " + seconds,
+                ChatColor.DARK_RED + "Zombies: " + zombieCount
+        );
+    }
 }
