5d6ea335b608bac1e99345318170d6bfa566ee97
NewAmazingPVP <gamingofpat@gmail.com>
2023-07-10 23:39:04 -0400

    balance weaponms fire rate and dmg

--- src/main/java/mc/minigame/variables/Weapons.java
+++ src/main/java/mc/minigame/variables/Weapons.java
@@ -9,79 +9,49 @@ public class Weapons {
 
     public static List<Material> weaponList = Arrays.asList(
             Material.WOODEN_HOE,
+            Material.WOODEN_SHOVEL,
             Material.STONE_HOE,
-            Material.IRON_HOE,
-            Material.GOLDEN_HOE,
-            Material.DIAMOND_HOE,
-            Material.NETHERITE_HOE,
-            Material.STONE_AXE,
-            Material.IRON_AXE,
-            Material.GOLDEN_AXE,
-            Material.DIAMOND_AXE,
-            Material.WOODEN_SWORD,
-            Material.STONE_SWORD,
-            Material.IRON_SWORD,
-            Material.GOLDEN_SWORD,
-            Material.DIAMOND_SWORD,
-            Material.NETHERITE_SWORD,
             Material.STONE_SHOVEL,
+            Material.SHEARS,
+            Material.IRON_HOE,
             Material.IRON_SHOVEL,
+            Material.GOLDEN_HOE,
             Material.GOLDEN_SHOVEL,
-            Material.DIAMOND_SHOVEL,
-            Material.NETHERITE_SHOVEL
+            Material.GOLDEN_PICKAXE,
+            Material.FLINT_AND_STEEL,
+            Material.DIAMOND_HOE,
+            Material.DIAMOND_PICKAXE
     );
 
 
     public static double getCooldownDuration(Material weaponType) {
         switch (weaponType) {
-            // Handguns
-            case WOODEN_HOE: // Glock
-                return 0.6; // 0.6 seconds
-            case STONE_HOE: // Beretta 92
-                return 0.8; // 0.8 seconds
-            case IRON_HOE: // Smith & Wesson M&P
-                return 1.0; // 1.0 seconds
-            case GOLDEN_HOE: // Sig Sauer P320
-                return 0.5; // 0.5 seconds
-            case DIAMOND_HOE: // Bolt-Action Rifle (Remington 700)
-                return 1.2; // 1.2 seconds
-            case NETHERITE_HOE: // Semi-Automatic Rifle (AR-15)
-                return 1.5; // 1.5 seconds
-            case WOODEN_AXE: // Pump-Action Shotgun (Remington 870)
-                return 1.0; // 1.0 seconds
-            case STONE_AXE: // Semi-Automatic Shotgun (Benelli M2)
-                return 1.2; // 1.2 seconds
-            case IRON_AXE: // Light Machine Gun (M249 SAW)
-                return 1.5; // 1.5 seconds
-            case GOLDEN_AXE: // Medium Machine Gun (M240)
-                return 2.0; // 2.0 seconds
-            case DIAMOND_AXE: // Heavy Machine Gun (Browning M2)
-                return 2.5; // 2.5 seconds
-            case WOODEN_SWORD: // Machete
-                return 0.8; // 0.8 seconds
-            case STONE_SWORD: // Katana
-                return 1.0; // 1.0 seconds
-            case IRON_SWORD: // Longsword
-                return 1.2; // 1.2 seconds
-            case GOLDEN_SWORD: // Rapier
-                return 0.6; // 0.6 seconds
-            case DIAMOND_SWORD: // Claymore
-                return 1.5; // 1.5 seconds
-            case NETHERITE_SWORD: // Zweihander
-                return 1.8; // 1.8 seconds
-            case WOODEN_SHOVEL: // Derringer
-                return 0.7; // 0.7 seconds
-            case STONE_SHOVEL: // Lever-Action Rifle
-                return 0.9; // 0.9 seconds
-            case IRON_SHOVEL: // Bolt-Action Shotgun
-                return 1.2; // 1.2 seconds
-            case GOLDEN_SHOVEL: // Sawed-Off Shotgun
-                return 0.5; // 0.5 seconds
-            case DIAMOND_SHOVEL: // Pump-Action Shotgun
-                return 1.5; // 1.5 seconds
-            case NETHERITE_SHOVEL: // Semi-Automatic Rifle
-                return 2.0; // 2.0 seconds
-
+            case WOODEN_HOE: // Knife
+                return 0.5;
+            case WOODEN_SHOVEL: // Pistol
+                return 0.5;
+            case STONE_HOE: // Ultimate Pistol
+                return 0.4;
+            case STONE_SHOVEL: // Rifle
+                return 0.2;
+            case SHEARS: // Ultimate Rifle
+                return 0.2;
+            case IRON_HOE: // Shotgun
+                return 1.4;
+            case IRON_SHOVEL: // Ultimate Shotgun
+                return 1.0;
+            case GOLDEN_HOE: // Rocket Launcher
+                return 2.0;
+            case GOLDEN_SHOVEL: // Ultimate Rocket Launcher
+                return 1.5;
+            case GOLDEN_PICKAXE: // Sniper
+                return 1.5;
+            case FLINT_AND_STEEL: // Ultimate Sniper
+                return 1.5;
+            case DIAMOND_HOE: // Elder Gun
+                return 0.9;
+            case DIAMOND_PICKAXE: // Ultimate Elder Gun
+                return 0.8;
             default:
                 return 0.0;
         }
@@ -89,59 +59,37 @@ public class Weapons {
 
     public static double calculateDamageAmount(Material weaponType) {
         switch (weaponType) {
-            // Handguns
-            case WOODEN_HOE: // Glock
-                return 8.0 / 2.0;
-            case STONE_HOE: // Beretta 92
-                return 10.0 / 2.0;
-            case IRON_HOE: // Smith & Wesson M&P
-                return 12.0 / 2.0;
-            case GOLDEN_HOE: // Sig Sauer P320
-                return 6.0 / 2.0;
-            case DIAMOND_HOE: // Bolt-Action Rifle (Remington 700)
-                return 25.0 / 2.0;
-            case NETHERITE_HOE: // Semi-Automatic Rifle (AR-15)
-                return 20.0 / 2.0;
-            case WOODEN_AXE: // Pump-Action Shotgun (Remington 870)
-                return 18.0 / 2.0;
-            case STONE_AXE: // Semi-Automatic Shotgun (Benelli M2)
-                return 16.0 / 2.0;
-            case IRON_AXE: // Light Machine Gun (M249 SAW)
-                return 10.0 / 2.0;
-            case GOLDEN_AXE: // Medium Machine Gun (M240)
-                return 8.0 / 2.0;
-            case DIAMOND_AXE: // Heavy Machine Gun (Browning M2)
-                return 35.0 / 2.0;
-            case WOODEN_SWORD: // Machete
-                return 12.0 / 2.0;
-            case STONE_SWORD: // Katana
-                return 15.0 / 2.0;
-            case IRON_SWORD: // Longsword
-                return 18.0 / 2.0;
-            case GOLDEN_SWORD: // Rapier
-                return 9.0 / 2.0;
-            case DIAMOND_SWORD: // Claymore
-                return 22.0 / 2.0;
-            case NETHERITE_SWORD: // Zweihander
-                return 25.0 / 2.0;
-            case WOODEN_SHOVEL: // Derringer
-                return 5.0 / 2.0;
-            case STONE_SHOVEL: // Lever-Action Rifle
-                return 8.0 / 2.0;
-            case IRON_SHOVEL: // Bolt-Action Shotgun
-                return 10.0 / 2.0;
-            case GOLDEN_SHOVEL: // Sawed-Off Shotgun
-                return 3.0 / 2.0;
-            case DIAMOND_SHOVEL: // Pump-Action Shotgun
-                return 16.0 / 2.0;
-            case NETHERITE_SHOVEL: // Semi-Automatic Rifle
-                return 20.0 / 2.0;
-
+            case WOODEN_HOE: // Knife
+                return 6.0;
+            case WOODEN_SHOVEL: // Pistol
+                return 6.0;
+            case STONE_HOE: // Ultimate Pistol
+                return 6.0;
+            case STONE_SHOVEL: // Rifle
+                return 6.0;
+            case SHEARS: // Ultimate Rifle
+                return 8.0;
+            case IRON_HOE: // Shotgun
+                return 4.5 * 10;
+            case IRON_SHOVEL: // Ultimate Shotgun
+                return 4.5 * 10;
+            case GOLDEN_HOE: // Rocket Launcher
+                return 15.0;
+            case GOLDEN_SHOVEL: // Ultimate Rocket Launcher
+                return 20.0;
+            case GOLDEN_PICKAXE: // Sniper
+                return 20.0;
+            case FLINT_AND_STEEL: // Ultimate Sniper
+                return 30.0;
+            case DIAMOND_HOE: // Elder Gun
+                return 15.0;
+            case DIAMOND_PICKAXE: // Ultimate Elder Gun
+                return 20.0;
             default:
                 return 0.0;
         }
     }
 
-    public Weapons(){
+    public Weapons() {
     }
 }
