26e16d043a5018469adf1f0ec7638f8be05d8a76
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-07-12 12:14:50 -0400

    push

--- src/main/java/mc/minigame/listener/WaterDamage.java
+++ src/main/java/mc/minigame/listener/WaterDamage.java
@@ -1,15 +1,10 @@
 package mc.minigame.listener;
 
-import mc.minigame.game.PlayerMoney;
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.inventory.ItemStack;
-import static mc.minigame.Zombies.zombies;
 
 public class WaterDamage implements Listener {
 
