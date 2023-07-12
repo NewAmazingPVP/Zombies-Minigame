18e343144d36ce64815acda62abc5a3b8f91d1a9
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-07-12 11:41:10 -0400

    Update PigCommand.java
    1

--- src/main/java/mc/minigame/command/PigCommand.java
+++ src/main/java/mc/minigame/command/PigCommand.java
@@ -13,24 +13,3 @@ public class PigCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("spawnpig")) {
-            if (args.length == 2) {
-                try {
-                    Player player = Bukkit.getPlayer(args[0]);
-                    if (player != null) {
-                        int amount = Integer.parseInt(args[1]);
-                        PlayerMoney.addCoins(player, amount);
-                        sender.sendMessage("Successfully gave money to " + player.getName());
-                    } else {
-                        sender.sendMessage("Player not found.");
-                    }
-                } catch (NumberFormatException e) {
-                    sender.sendMessage("Invalid amount specified.");
-                }
-            } else {
-                sender.sendMessage("Please specify a player and an amount.");
-            }
-        }
-
-        return true;
-    }
-}
