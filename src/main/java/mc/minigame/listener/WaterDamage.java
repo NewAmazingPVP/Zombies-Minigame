7fb6ad792bf70c68c8a895748281389cd7f1bc29
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-07-12 12:14:21 -0400

    push

--- src/main/java/mc/minigame/listener/WaterDamage.java
+++ src/main/java/mc/minigame/listener/WaterDamage.java
@@ -3,9 +3,7 @@ package mc.minigame.listener;
 import mc.minigame.game.PlayerMoney;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,6 +14,15 @@ import static mc.minigame.Zombies.zombies;
 public class WaterDamage implements Listener {
 
     @EventHandler
-    public void onPlayerMove(PlayerMoveEvent event
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        Location to = event.getTo();
+        World world = player.getWorld();
 
+        if (to.getBlock().getType() == Material.WATER && to.getBlock().getRelative(0, -1, 0).getType() != Material.WATER) {
+            // Player stepped into water
+            player.damage(2.0);
+            player.sendMessage("You took damage from stepping into water!");
+        }
     }
+}
