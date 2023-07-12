0e6d7f58984adb07c62feced8816e79e1f523e34
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-07-12 12:29:11 -0400

    Update WaterDamage.java

--- src/main/java/mc/minigame/listener/WaterDamage.java
+++ src/main/java/mc/minigame/listener/WaterDamage.java
@@ -1,11 +1,9 @@
 package mc.minigame.listener;
 
-import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityToggleSwimEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 
 public class WaterDamage implements Listener {
 
