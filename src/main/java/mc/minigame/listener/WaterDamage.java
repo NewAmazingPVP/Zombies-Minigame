42281de0e62f81abbb99b6e5f574363407c04ab0
NewAmazingPVP <gamingofpat@gmail.com>
2023-07-12 12:35:08 -0400

    push

--- src/main/java/mc/minigame/listener/WaterDamage.java
+++ src/main/java/mc/minigame/listener/WaterDamage.java
@@ -12,15 +12,6 @@ import org.bukkit.event.player.PlayerMoveEvent;
 public class WaterDamage implements Listener {
 
     @EventHandler
-    public void onPlayerMove(EntityToggleSwimEvent event) {
-        if (event.getEntity() instanceof Player) {
-            Player player = (Player) event.getEntity();
-            player.damage(2.0);
-            player.sendMessage("[\"\",{\"text\":\"\\u26a0 \\u2622 \",\"color\":\"dark_red\"},{\"text\":\"WARNING WATER RADIOACTIVE\",\"bold\":true,\"color\":\"dark_red\"},{\"text\":\" \\u2622 \\u26a0\",\"color\":\"dark_red\"}]");
-        }
-    }
-
-    @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
         Location to = event.getTo();
