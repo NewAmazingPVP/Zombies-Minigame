76c4fe295e22ae47926ac1e65568e4c2583d5bce
NewAmazingPVP <gamingofpat@gmail.com>
2023-07-13 19:07:35 -0400

    fixes

--- src/main/java/mc/minigame/variables/Loc.java
+++ src/main/java/mc/minigame/variables/Loc.java
@@ -4,7 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
 public class Loc {
-    public static Location mapEdgeloc1 = new Location(Bukkit.getWorld("world"), 50.0, -21.0, -120.0);
+    public static Location mapEdgeloc1 = new Location(Bukkit.getWorld("world"), 50.0, -20.0, -120.0);
     public static Location mapEdgeloc2 = new Location(Bukkit.getWorld("world"), -40.0, -4.0, 10.0);
     public static Location deadLobby = new Location(Bukkit.getWorld("world"), -2.5, 55.0, -48.5);
     public static Location startLoc = new Location(Bukkit.getWorld("world"), -38.0, -19.0, -58.0);
