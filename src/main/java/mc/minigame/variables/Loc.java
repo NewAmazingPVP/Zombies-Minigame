40c1e81af294c72175c9bd6553805e549561efc6
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-07-13 12:10:48 -0400

    fixes

--- src/main/java/mc/minigame/variables/Loc.java
+++ src/main/java/mc/minigame/variables/Loc.java
@@ -9,5 +9,5 @@ public class Loc {
     public static Location deadLobby = new Location(Bukkit.getWorld("world"), -2.5, 55.0, -48.5);
     public static Location startLoc = new Location(Bukkit.getWorld("world"), -38.0, -19.0, -58.0);
     public static Location lobby = new Location(Bukkit.getWorld("world"), -220.5, -48.0, -246.5);
-    public static Location jail = new Location(Bukkit.getWorld("wodld"), -38, -19, -92);
+    public static Location jail = new Location(Bukkit.getWorld("world"), -38, -19, -92);
 }
