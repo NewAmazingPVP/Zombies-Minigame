67bf23221753ac392f006e3f3520d0ed5a50d100
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-07-13 13:29:10 -0400

    more changes

--- src/main/java/mc/minigame/variables/Weapons.java
+++ src/main/java/mc/minigame/variables/Weapons.java
@@ -1,6 +1,8 @@
 package mc.minigame.variables;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.inventory.EquipmentSlot;
 
 import java.util.Arrays;
 import java.util.List;
@@ -59,7 +61,7 @@ public class Weapons {
 
     public static double calculateDamageAmount(Material weaponType) {
         switch (weaponType) {
-            case WOODEN_HOE: // Pistol I
+            case WOODEN_HOE: // Pistol
                 return 6.0;
             case WOODEN_SHOVEL: // Pistol I
                 return 6.0;
@@ -83,12 +85,46 @@ public class Weapons {
                 return 2.0;
             case DIAMOND_HOE: // Venerable Gun
                 return 15.0;
-            case DIAMOND_PICKAXE: // Superb Sniper Gun
+            case DIAMOND_PICKAXE: // Superb Venerable Gun
                 return 30.0;
             default:
                 return 0.0;
         }
     }
+
+    public static String getName(Material weaponType) {
+        switch (weaponType) {
+            case WOODEN_HOE:
+                return ChatColor.GREEN + "Pistol";
+            case WOODEN_SHOVEL:
+                return ChatColor.YELLOW + "Pistol I";
+            case STONE_HOE:
+                return ChatColor.GOLD + "Supreme Pistol";
+            case STONE_SHOVEL:
+                return ChatColor.GREEN + "Rifle";
+            case SHEARS:
+                return ChatColor.YELLOW + "Superb Rifle";
+            case IRON_HOE:
+                return ChatColor.RED + "Shotgun";
+            case IRON_SHOVEL:
+                return ChatColor.DARK_RED + "Ultimate Shotgun";
+            case GOLDEN_HOE:
+                return ChatColor.LIGHT_PURPLE + "Rocket Launcher";
+            case GOLDEN_SHOVEL:
+                return ChatColor.DARK_PURPLE + "Supreme Rocket Launcher";
+            case GOLDEN_PICKAXE:
+                return ChatColor.AQUA + "Sniper";
+            case FLINT_AND_STEEL:
+                return ChatColor.RED + "Flamethrower";
+            case DIAMOND_HOE:
+                return ChatColor.BLUE + "Venerable Gun";
+            case DIAMOND_PICKAXE:
+                return ChatColor.DARK_BLUE + "Superb Venerable Gun";
+            default:
+                return ChatColor.GRAY + "Unknown Weapon";
+        }
+    }
+
 
     public Weapons() {
     }
